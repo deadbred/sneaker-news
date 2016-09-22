f9db7a4e17a641bf59b7f60c333655c5785f7b94
deadbred <deadbred3@gmail.com>
2016-09-22 14:58:30 -0400

    Created Routes and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
+
+  resources :posts, only: [:create, :index, :show] do
+  	resources :comments, only: [:show, :create] do
+  		member do
+  			put '/upvotes' => 'comments#upvotes'
+  		end
+  	end
+
+  	member do
+  		put '/upvotes' => 'posts#upvote'
+  	end
+  end
 end
